874ee1dee819cf2f424dabd654dba9576d15bb6e
voshev <55592033+voshev@users.noreply.github.com>
2020-11-09 07:02:44 +0000

    fixed solar_input

--- solar_input.py
+++ solar_input.py
@@ -89,7 +89,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     **output_filename** — имя входного файла
     **space_objects** — список объектов планет и звёзд
     """
- with open(output_filename, 'w') as out_file:
+    with open(output_filename, 'w') as out_file:
         for obj in space_objects:
             print(out_file, "%s %f %s %f, %f, %f, %f, %f" % (obj.tipe, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy,))
 
